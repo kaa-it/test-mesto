b98eb23be6a66ada46ae0651457e477b6c3544c4
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2022-10-27 16:26:08 +0300

    массив

--- js/main.js
+++ js/main.js
@@ -1,49 +1,52 @@
 const initialCards = [
   {
-    id: "card1new",
     name: "Архыз",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    id: "card2new",
     name: "Челябинская область",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    id: "card3new",
     name: "Иваново",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    id: "card4new",
     name: "Камчатка",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    id: "card5new",
     name: "Холмогорский район",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    id: "card6new",
     name: "Байкал",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
 
-const cardOne = document.querySelector("#card1").setAttribute("src", initialCards[0].link);
-const cardTwo = document.querySelector("#card2").setAttribute("src", initialCards[1].link);
-const cardThree = document.querySelector("#card3").setAttribute("src", initialCards[2].link);
-const cardFour = document.querySelector("#card4").setAttribute("src", initialCards[3].link);
-const cardFive = document.querySelector("#card5").setAttribute("src", initialCards[4].link);
-const cardSix = document.querySelector("#card6").setAttribute("src", initialCards[5].link);
+initialCards.forEach(function (item) {
+  const container = document.querySelector(".elements"); //.elements
+  const elementTemplate = document.querySelector("#card-template").content; //.element
+  const elementCopy = elementTemplate.querySelector(".element").cloneNode(true);
+  container.prepend(elementCopy);
+  const card = container;
 
-const titleOne = (document.querySelector("#title1").textContent = initialCards[0].name);
-const titleTwo = (document.querySelector("#title2").textContent = initialCards[1].name);
-const titleThree = (document.querySelector("#title3").textContent = initialCards[2].name);
-const titleFour = (document.querySelector("#title4").textContent = initialCards[3].name);
-const titleFive = (document.querySelector("#title5").textContent = initialCards[4].name);
-const titleSix = (document.querySelector("#title6").textContent = initialCards[5].name);
+  card.querySelector(".like").setAttribute("src", "images/Group.svg");
+  card.querySelector(".element__vector").addEventListener("click", likes, true);
+  card.querySelector(".element__delete").addEventListener("click", deleteCard, true);
+
+  const standartCard = item;
+  console.log("что содержится в переменной" + item);
+
+  const ImageName = standartCard.name;
+  const ImageSrc = standartCard.link;
+
+  const titleOneNew = (document.querySelector(".element__title").textContent = ImageName);
+  const linkCardNew = document.querySelector(".element__image-element").setAttribute("src", ImageSrc);
+  const linkCardAlt = document.querySelector(".element__image-element").setAttribute("alt", ImageName);
+  card.querySelector(".element__picture").addEventListener("click", popupOpenPicture, true);
+});
 
 const popupProfile = document.querySelector("#profile-edit");
 const editProfile = document.querySelector(".profile__edit-button").addEventListener("click", popupOpen, true);
@@ -95,14 +98,13 @@ function closePopupNew(e) {
 const elementTitle = document.querySelector(".element__title");
 const linkCard = document.querySelector(".element__image-element");
 const submitCreate = document.querySelector("#create").addEventListener("click", createCard, true);
-
 function createCard(e) {
   const container = document.querySelector(".elements");
   const elementTemplate = document.querySelector("#card-template").content;
   const elementCopy = elementTemplate.querySelector(".element").cloneNode(true);
   container.prepend(elementCopy);
-
   const card = container;
+
   card.querySelector(".like").setAttribute("src", "images/Group.svg");
   card.querySelector(".element__vector").addEventListener("click", likes, true);
   card.querySelector(".element__delete").addEventListener("click", deleteCard, true);
